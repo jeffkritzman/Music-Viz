4e7deffd7bfbacc6c29632c5d70d8907dc3628c1
Jeffrey Kritzman <24811122+jeffkritzman@users.noreply.github.com>
2019-07-17 15:53:47 -0500

    Add sentiment analysis
    Added sentiment analysis, and word counts
    Also, in previous version, added functionality to lookup audio and lyric files on google drive, and download if needed.  This file list is used to automate the rest of the script

--- music viz 2.R
+++ music viz 2.R
@@ -10,8 +10,8 @@ download.files = 0 #if need to get files, set to 1. else, set to 0.
 num.points = 100
 freq.mod = 7
 # set working directory
-#setwd("H:/R/Music Viz")
-setwd("~/R/music viz")
+setwd("H:/R/Music Viz 2")
+#setwd("~/R/music viz")
 
 
 ################################################################
@@ -37,14 +37,16 @@ slug.list <- as.vector(rep("", nrow(mp3.list)))
 mp3.list <- mp3.list[order(mp3.list$name),]
 lyric.list <- lyric.list[order(lyric.list$name),]
 
-################################################################
-#
-# Set up spectrogram function
-#
-################################################################
-
-spectrogram <- function(mp3, num.points = 100, freq.mod = 7) { 
-  # read in audio file
+for (i in 1:nrow(mp3.list)) {
+  slug.list[i] <- str_replace(mp3.list[i, "name"], ".mp3", "")
+  mp3 <- mp3.list[i, ]
+  lyrics <- lyric.list[i, ]
+  if (download.files == 1) {
+    drive_download(mp3, overwrite = TRUE)
+    drive_download(lyrics, overwrite = TRUE)
+  }
+  
+  #start spectrogram processing
   mp3.name <- as.character(mp3["name"])
   if (str_detect(tolower(mp3.name),'.mp3')) {
     freq.data = readMP3(mp3.name)
@@ -73,32 +75,52 @@ spectrogram <- function(mp3, num.points = 100, freq.mod = 7) {
   P1.ds <- as.data.frame(z1)
   colnames(P1.ds) <- f1
   P1.ds$t <- t1
-  dim(P1.ds)
   #trim file: keep only num.points moments in time, evenly distributed through song
   r <- 1:nrow(P1.ds)
   mod <- round(nrow(P1.ds) / num.points)
   P1.ds <- P1.ds[r%%mod == 0,]
+  #trim frequency
+  c <- 1:ncol(P1.ds)
+  P1.ds <- P1.ds[ , c%%freq.mod == 0]
+  f1 <- f1[ c%%freq.mod == 0]
+  t1 <- t1[r%%mod == 0]
+  colnames(P1.ds) <- f1
+  P1.ds$t <- t1
   
   #gather data frame and output to file
   gat1 <- gather(P1.ds, key = "freq", value = "level", -t) #pivot, i.e. make tidy
-  write.csv(gat1, file = str_replace(mp3.list[i, "name"], ".mp3", ".csv"), row.names = FALSE)
+  write.csv(gat1, file = str_replace(mp3.list[i, "name"], ".mp3", "_spectrogram.csv"), row.names = FALSE)
 }
 
+# Initialize libraries, pt 2. this avoid conflicting variable names
+library(textdata)
+library(tidytext)
+library(dplyr)
 
+#get word sentiment scores
+afinn.sentiment <- get_sentiments("afinn")
 
-for (i in 1:nrow(mp3.list)) {
-  slug.list[i] <- str_replace(mp3.list[i, "name"], ".mp3", "")
-  mp3 <- mp3.list[i, ]
+for (i in 1:nrow(lyric.list)) {
   lyrics <- lyric.list[i, ]
-  if (download.files == 1) {
-    drive_download(mp3, overwrite = TRUE)
-    drive_download(lyrics, overwrite = TRUE)
-  }
-  spectrogram(mp3 = mp3, num.points = num.points, freq.mod = freq.mod)
+  #build 'tidy' lyrics and save as csv
+  tidy.text <- read.delim(as.character(lyrics["name"]), header = FALSE)
+  tidy.text <- tibble(line = 1:nrow(tidy.text), lyrics.line = as.character(tidy.text$V1))
+  tidy.text <- tidy.text %>% unnest_tokens(word, lyrics.line) 
+  tidy.text <- tidy.text %>% count(word, sort = TRUE)
+  tidy.text <- left_join(tidy.text, afinn.sentiment)
+  tidy.text[is.na(tidy.text)] <- 0
+  names(tidy.text) <- c("word", "occurrences", "sentiment_value")
+  write.csv(tidy.text, file = str_replace(lyrics["name"], ".txt", "_tidy_lyrics.csv"), row.names = FALSE)
 }
+
+################################################################
+#
+# Gather and write meta-data
+#
+################################################################
 
 meta.data <- as.data.frame(slug.list)
+names(meta.data) <- "Song"
 meta.data$mp3.file <- mp3.list$name
 meta.data$lyrics.file <- lyric.list$name
-meta.data
-
+write.csv(meta.data, file = "metadata.csv", row.names = FALSE)
